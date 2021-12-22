eaa8ca9e63f5f35a6fe1d6f11e30beee52785ee0
Redouane Bouabana <rredouane342@gmail.com>
2021-12-22 11:36:32 +0100

    login admin

--- api/Models/Auth.js
+++ api/Models/Auth.js
@@ -2,7 +2,7 @@ const con = require("../../config/db");
 class Auth {
   static findAllAdmins = () => {
     return new Promise((resolve, reject) => {
-      con.query(`SELECT * FROM admin_center`, (err, result) => {
+      con.query(`SELECT * FROM admin`, (err, result) => {
         if (err) throw err;
         resolve(result);
       });
