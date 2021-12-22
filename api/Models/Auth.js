327916281d1175a997fe6fbaef1672156832bce1
Redouane Bouabana <rredouane342@gmail.com>
2021-12-22 22:35:49 +0100

    suii

--- api/Models/Auth.js
+++ api/Models/Auth.js
@@ -9,48 +9,6 @@ class Auth {
       });
     });
   };
-
-  static findAllCenterAdmins = () => {
-    return new Promise((resolve, reject) => {
-      con.query(`SELECT * FROM admin_center`, (err, result) => {
-        if (err) throw err;
-        resolve(result);
-      });
-    });
-  };
-  
-
-  static create = async (admin_center) => {
-    con.query("INSERT INTO admin_center SET ?", {
-      firstName: admin_center.firstName,
-      lastName: admin_center.lastName,
-      email: admin_center.email,
-      password: admin_center.password,
-      token: admin_center.token,
-    });
-  };
-  static createRayonAdmin = async (admin_rayon) => {
-    con.query("INSERT INTO chef_rayon SET ?", {
-      firstName: admin_rayon.firstName,
-      lastName: admin_rayon.lastName,
-      email: admin_rayon.email,
-      id_admin_center: admin_rayon.id_admin_center,
-      password: admin_rayon.password,
-      rayon: admin_rayon.rayon,
-      token: admin_rayon.token,
-    });
-  };
-
-  static update = async (token, id) => {
-    con.query(`UPDATE admin_center SET ? WHERE id =${id}`, {
-      token: token,
-    });
-  };
-  static updatePassword = async (password, id) => {
-    con.query(`UPDATE admin_center SET ? WHERE id =${id}`, {
-      password: password,
-    });
-  };
 }
 
 module.exports = Auth;
