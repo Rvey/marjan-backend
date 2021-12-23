ef1833be39cd435c9719c7a029c630435be49960
Redouane Bouabana <rredouane342@gmail.com>
2021-12-23 16:17:07 +0100

    update db | cahierdcharge

--- sql/marjane.sql
+++ sql/marjane.sql
@@ -13,8 +13,9 @@ CREATE TABLE `admin` (
   `lastName` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `admin_center` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -22,22 +23,23 @@ CREATE TABLE `admin_center` (
   `lastName` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `chef_rayon` (
   `id` int NOT NULL AUTO_INCREMENT,
   `firstName` varchar(100) DEFAULT NULL,
   `lastName` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `pays` varchar(100) DEFAULT NULL,
   `id_admin_center` int DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `rayon` varchar(100) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_admin_center` (`id_admin_center`),
-  CONSTRAINT `chef_rayon_ibfk_1` FOREIGN KEY (`id_admin_center`) REFERENCES `admin_center` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `chef_rayon_ibfk_1` FOREIGN KEY (`id_admin_center`) REFERENCES `admin_center` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -47,64 +49,60 @@ CREATE TABLE `products` (
   `price` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_rayon` (`id_rayon`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_rayon`) REFERENCES `rayons` (`id`)
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_rayon`) REFERENCES `rayon` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `promotions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `promotion` varchar(100) DEFAULT NULL,
-  `id_chef_rayon` int DEFAULT NULL,
-  `id_produit` int DEFAULT NULL,
+  `id_admin_center` int DEFAULT NULL,
+  `id_product` int DEFAULT NULL,
+  `id_rayon` int DEFAULT NULL,
   `date_promotion` varchar(100) DEFAULT NULL,
   `status` varchar(10) DEFAULT 'false',
   PRIMARY KEY (`id`),
-  KEY `id_chef_rayon` (`id_chef_rayon`),
-  KEY `id_produit` (`id_produit`),
-  CONSTRAINT `promotions_ibfk_1` FOREIGN KEY (`id_chef_rayon`) REFERENCES `chef_rayon` (`id`),
-  CONSTRAINT `promotions_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `id_rayon` (`id_rayon`),
+  KEY `id_product` (`id_product`),
+  KEY `id_admin_center` (`id_admin_center`),
+  CONSTRAINT `promotions_ibfk_1` FOREIGN KEY (`id_rayon`) REFERENCES `rayon` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `promotions_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `promotions_ibfk_3` FOREIGN KEY (`id_admin_center`) REFERENCES `admin_center` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `rayons` (
+CREATE TABLE `rayon` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) DEFAULT NULL,
   `id_chef_rayon` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id_chef_rayon` (`id_chef_rayon`),
+  CONSTRAINT `rayon_ibfk_1` FOREIGN KEY (`id_chef_rayon`) REFERENCES `chef_rayon` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
-(1, 'HHHHH', 'undefined', 'red@gmail.com', 'undefined');
-INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
-(4, 'HHHHH', 'redoneww', 'user.email', 'user.phone');
-INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
-(5, 'HHHHH', 'redoneww', 'user.email', 'user.phone');
-INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
-(6, 'yep cock', 'redoneww', 'user.email', 'user.phone'),
-(7, 'yep coeeeeck', 'redoneww', 'emaildddd@gmail.com', 'password');
-
-INSERT INTO `admin_center` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES
-(1, 'chef_center', 'redone', 'red@gmail.com', '1234');
+INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `password`, `token`) VALUES
+(1, ' redone', 'ehehe', 'red@gmail.com', '1234', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjQwMjU1NDU2LCJleHAiOjE2NDAyNjI2NTZ9.s0QctVMNJmaYvBIrU8VYunyFWma_TqVwHpxMxKhCCPM');
 
 
-INSERT INTO `chef_rayon` (`id`, `firstName`, `lastName`, `email`, `pays`, `id_admin_center`, `password`, `rayon`) VALUES
-(1, 'chef_rayon', 'redone ', 'chef@gmail.com', '2332', 1, '1234', 'electronique');
+INSERT INTO `admin_center` (`id`, `firstName`, `lastName`, `email`, `password`, `token`) VALUES
+(2, 'admincenter', 'redone', 'admincen43443ter12@gmail.com', '1234', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjQwMjE0NDYyLCJleHAiOjE2NDAyMjE2NjJ9.QciAwhKhJWqpH8d44N7_J4MgSv7s7kvSIYVoKvxYDJU');
+
+
+INSERT INTO `chef_rayon` (`id`, `firstName`, `lastName`, `email`, `id_admin_center`, `password`, `rayon`, `token`) VALUES
+(4, 'simo', 'chefRayonLastName', 'simochefrayon@gmail.com', 2, '1234', 'electronic', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjQwMjA3ODM0LCJleHAiOjE2NDAyMTUwMzR9.Sooi_LoFWU5T0mU0Qc28iBltsHveK9pmAlxzfwWHaLs');
 
 
 INSERT INTO `products` (`id`, `nom`, `id_rayon`, `quantity`, `price`) VALUES
-(1, 'clavier', 1, 12, '50');
+(1, 'bimo', 1, 50, '1');
 
 
-INSERT INTO `promotions` (`id`, `promotion`, `id_chef_rayon`, `id_produit`, `date_promotion`, `status`) VALUES
-(2, '5020%', 1, 1, '21-01-2022', 'false');
-INSERT INTO `promotions` (`id`, `promotion`, `id_chef_rayon`, `id_produit`, `date_promotion`, `status`) VALUES
-(3, '5%', 1, 1, '21-01-2022', 'false');
-INSERT INTO `promotions` (`id`, `promotion`, `id_chef_rayon`, `id_produit`, `date_promotion`, `status`) VALUES
-(4, '5020%', 1, 1, '21-01-2022', 'false');
-INSERT INTO `promotions` (`id`, `promotion`, `id_chef_rayon`, `id_produit`, `date_promotion`, `status`) VALUES
-(5, NULL, 1, 1, '21-01-2022', 'false'),
-(6, NULL, 1, 1, '21-01-2022', 'false');
+INSERT INTO `promotions` (`id`, `promotion`, `id_admin_center`, `id_product`, `id_rayon`, `date_promotion`, `status`) VALUES
+(2, '50%', 2, 1, NULL, '22-12-2021', 'Processed');
+INSERT INTO `promotions` (`id`, `promotion`, `id_admin_center`, `id_product`, `id_rayon`, `date_promotion`, `status`) VALUES
+(3, '50%', 2, 1, NULL, '22-12-2021', 'Processed');
+INSERT INTO `promotions` (`id`, `promotion`, `id_admin_center`, `id_product`, `id_rayon`, `date_promotion`, `status`) VALUES
+(4, '50%', 2, 1, 1, '22-12-2021', 'Processed');
 
-INSERT INTO `rayons` (`id`, `nom`, `id_chef_rayon`) VALUES
-(1, 'electronique', 1);
+INSERT INTO `rayon` (`id`, `nom`, `id_chef_rayon`) VALUES
+(1, 'electronic', 4);
 
 
 
