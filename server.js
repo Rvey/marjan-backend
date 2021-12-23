9b2500de4466541c6dcb883b769691fbdd0ce3a8
Redouane Bouabana <rredouane342@gmail.com>
2021-12-23 11:39:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,27 +5,7 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 8082;
 
-// var corsOptions = {
-//   origin: `http://localhost:${PORT}`
-// };
-
-// app.use(cors(corsOptions));
-
-
-
 app.use(express.urlencoded({ extended: true }));
-// cookie parser middleware
-// app.use(cookieParser());
-
-
-// const oneDay = 1000 * 60 * 60 * 24;
-// //session middleware
-// app.use(sessions({
-//   secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-//   saveUninitialized:false,
-//   cookie: { maxAge: oneDay },
-//   resave: false
-// }));
 
 app.use(cors());
 app.use(express.json());
